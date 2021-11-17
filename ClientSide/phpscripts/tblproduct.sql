d336013d463e487cafde6cf7a24fd87e36b020cc
Happtomato <Dominik@fdo-net.ch>
2021-11-17 08:38:34 +0100

    Rnd Stuff

---
+++ ClientSide/phpscripts/tblproduct.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `inventory` (`ItemName`, `ItemCode`, `ItemImage`, `ItemQuantity`, `ItemPrice`) VALUES
+( 'Cola', '1001', '../Pictures/cola.png', 1000.00, 3.50),
+( 'Fanta', '1002' ,'../Pictures/Fanta.png', 2000.00, 3.50),
+( 'Fanta Blau', '1003', '../Pictures/FantaBlau.jpg', 500.00, 3.50),
+( 'Sprite', '1004', '../Pictures/', 800.00, 3.50);
+
+COMMIT;
