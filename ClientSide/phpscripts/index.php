fec4cb99232e38b6b219130ac1031787b6684532
Happtomato <Dominik@fdo-net.ch>
2021-11-18 10:30:35 +0100

    smol change

--- ClientSide/phpscripts/index.php
+++ ClientSide/phpscripts/index.php
@@ -132,27 +132,6 @@ if(!empty($_GET["action"])) {
 
 <!-- List of Products -->
 
-<div id="product-grid">
-    <div class="txt-heading">Products</div>
-    <?php
-    $product_array = $db_handle->runQuery("SELECT * FROM inventory ORDER BY Item_ID ASC");
-    if (!empty($product_array)) {
-        foreach($product_array as $key=>$value){
-            ?>
-            <div class="product-item">
-                <form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["ItemCode"]; ?>">
-                   <!-- <div class="product-image"><img src="<?php echo $product_array[$key]["ItemImage"]; ?>"></div> -->
-                    <div class="product-tile-footer">
-                        <div class="product-title"><?php echo $product_array[$key]["ItemName"]; ?></div>
-                        <div class="product-price"><?php echo $product_array[$key]["ItemPrice"]."fr"; ?></div>
-                        <div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
-                    </div>
-                </form>
-            </div>
-            <?php
-        }
-    }
-    ?>
-</div>
+
 </BODY>
 </HTML>
